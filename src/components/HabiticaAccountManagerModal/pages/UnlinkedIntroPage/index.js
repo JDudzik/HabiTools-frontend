14d4600cd59308b9be6c5e6e8bc1aa7ad25cad25
JDudzik <jdudzik950@gmail.com>
2026-05-20 10:25:18 -0400

    Update text for unlinked intro page

--- src/components/HabiticaAccountManagerModal/pages/UnlinkedIntroPage/index.js
+++ src/components/HabiticaAccountManagerModal/pages/UnlinkedIntroPage/index.js
@@ -5,15 +5,13 @@ import { L } from 'components';
 export const UnlinkedIntroPage = ({ openHowWeSecureModal, onNavigate }) => {
   return (
     <Stack spacing={ 3 }>
-      <L.div>
-        <L.p>
-          To use HabiTools, you need to link your Habitica account. This allows us to safely
-          connect your account so our tools can help automate your Habitica experience.
-        </L.p>
-        <L.p sx={{ fontSize: '0.9rem', color: 'text.secondary' }}>
-          We keep your information secure and encrypted. Learn more about our security practices below.
-        </L.p>
-      </L.div>
+      <L.p>
+        To use HabiTools, you need to link your Habitica account. This allows us to safely connect
+        your account so you can use the tools and automations.
+      </L.p>
+      <L.p sx={{ fontSize: '0.9rem', color: 'text.secondary' }}>
+        We keep your information secure and encrypted. Learn more about our security practices below.
+      </L.p>
 
       <Stack spacing={ 2 } direction="row" justifyContent="flex-end">
         <Button
